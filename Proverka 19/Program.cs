8faed1c1f586722f5172360fe0c945f04a6d6c4f
NikolayStanchev99 <bylgarian@gmail.com>
2024-02-16 20:48:14 +0200

    add proverka 1

---
+++ Proverka 19/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.WriteLine("Въведете възрастта си:");
+int age = int.Parse(Console.ReadLine());
+
+if (age > 18)
+{
+    Console.WriteLine("Пълнолетен!");
+}
+else
+{
+    Console.WriteLine("Непълнолетен!");
+}
